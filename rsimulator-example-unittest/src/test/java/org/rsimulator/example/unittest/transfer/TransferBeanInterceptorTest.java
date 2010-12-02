14296f19b9263c57a3cddb2dc03abcf4847fc9ec
Magnus Bjuvensjo <bjuvensjo@gmail.com>
2010-12-02 20:30:02 +0100

    Changed implementation for AspectJ to hav it work not only with Maven but also within Eclipse with AJDT.

--- rsimulator-example-unittest/src/test/java/org/rsimulator/example/unittest/transfer/TransferBeanInterceptorTest.java
+++ rsimulator-example-unittest/src/test/java/org/rsimulator/example/unittest/transfer/TransferBeanInterceptorTest.java
@@ -3,31 +3,31 @@ package org.rsimulator.example.unittest.transfer;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.junit.Test;
 import org.rsimulator.example.unittest.transfer.account.Account;
-import org.rsimulator.interceptor.AspectJSimulator;
+import org.rsimulator.interceptor.AspectJSimulatorAdapter;
 
 public class TransferBeanInterceptorTest {
     private TransferBean transferBean;
     
     @SuppressWarnings("unused")
     @Aspect
-    private static class TransferBeanInterceptorTestAspect extends AspectJSimulator {        
-        public TransferBeanInterceptorTestAspect() {
-            setRootPath(TransferBeanInterceptorTest.class);
-        }       
-
-        //@Pointcut("call(* org.rsimulator.example.unittest.transfer.account.AccountPort.getAccounts(..))")
-        @Pointcut("call(* TransferBean.*(..)) && within(TransferBeanInterceptorTest)")
-        protected void aspectJSimulatorPointcut() {            
-        }
+    private static class SimulatorAspect {   
+        //TODO Inject
+        private AspectJSimulatorAdapter aspectJSimulatorAdapter = new AspectJSimulatorAdapter();
         
-    }    
-    
+        @Around("call(* TransferBean.*(..)) && within(TransferBeanInterceptorTest)")
+        public Object invoke(ProceedingJoinPoint pjp) throws IOException {
+            return aspectJSimulatorAdapter.invoke(pjp, TransferBeanInterceptorTest.class, false);
+        }   
+    }
+      
     @Test
     public void testGetAccounts() {
         List<Account> accounts = transferBean.getAccounts("1111");
