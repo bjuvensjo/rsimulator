d83af84ef90292ba513c11fa709391c85a10b98f
Magnus Bjuvensjö <magnus.bjuvensjo@evry.com>
2021-11-29 21:08:19 +0100

    new file:   rsimulator-test/src/main/java/com/github/bjuvensjo/rsimulator/test/spock/Dummy.java

---
+++ rsimulator-test/src/main/java/com/github/bjuvensjo/rsimulator/test/spock/Dummy.java
@@ -0,0 +1,11 @@
+package com.github.bjuvensjo.rsimulator.test.spock;
+
+/**
+ * Only here to generate javadoc and thus satisfy Nexus staging rules.
+ */
+public class Dummy {
+    
+    public String foo() {
+        return "foo";
+    }
+}
